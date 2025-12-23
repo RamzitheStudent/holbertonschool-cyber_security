dcf0b76bf5bad10f18ebcc39baeed08df906aea3
RamzitheStudent <12554@holbertonstudents.com>
2025-12-23 11:10:51 +0000

    gm

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F: '/Registrant|Admin|Tech/ {gsub(/^ +/,"",$2); print ($1~/Street/?$1","$2" ":$1~/Ext/?$1":,":$1","$2)}' > "$1.csv"
+whois "$1" | awk -F: '/^(Registrant|Admin|Tech) (Name|Organization|Street|City|State\/Province|Postal Code|Country|Phone|Fax|Email)/{gsub(/^ +/,"",$2); printf "%s, %s\n",$1,($1~/(Street)/?$2" ":$2)} END{print "Registrant Phone Ext:, \nRegistrant Fax, \nRegistrant Fax Ext:, \nAdmin Phone Ext:, \nAdmin Fax, \nAdmin Fax Ext:, \nTech Phone Ext:, \nTech Fax, \nTech Fax Ext:, "}' > "$1.csv"
