a9afda49e937dc606b2d99f878401cd1b0eb226b
RamzitheStudent <12554@holbertonstudents.com>
2025-12-26 14:35:21 +0000

    gm

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk '/Registrant|Admin|Tech/ && /(Name|Organization|Street|City|State|Postal|Country|Phone|Fax|Email):/ {sub(/:[[:space:]]*/,", "); print}' > "$1.csv"
+whois "$1" | awk '/Registrant|Admin|Tech/ {if(match($0,/(Name|Organization|Street|City|State\/Province|Postal Code|Country|Phone Ext:|Phone|Fax Ext:|Fax|Email):/)){sub(/:[[:space:]]*/,", ");print}}' > "$1.csv"
