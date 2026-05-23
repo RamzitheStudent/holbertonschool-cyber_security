5859133131322ed13439be1b151936eb9cb7e675
RamzitheStudent <12554@holbertonstudents.com>
2026-05-23 14:13:50 -0400

    gm

--- scripting_cyber/0x00-ruby_scripting/8-post.rb
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -1,23 +1,19 @@
 #!/usr/bin/env ruby
 require 'net/http'
-require 'uri'
 require 'json'
+require 'uri'
 
 def post_request(url, body_params = {})
   uri = URI.parse(url)
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = (uri.scheme == "https")
 
-  # Create an HTTP POST request
-  request = Net::HTTP::Post.new(uri)
-  request.content_type = 'application/json'
-  request.body = body_params.to_json unless body_params.empty?
+  request = Net::HTTP::Post.new(uri.request_uri, { 'Content-Type' => 'application/json' })
+  request.body = body_params.to_json
 
-  # Initialize the HTTP session
-  response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
-    http.request(request)
-  end
+  response = http.request(request)
 
-  # Print response status and body
   puts "Response status: #{response.code} #{response.message}"
   puts "Response body:"
-  puts JSON.pretty_generate(JSON.parse(response.body))
+  puts response.body
 end
